536627c9416a3f0008a09f7f617b551defc88425
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-02-27 11:07:07 +0100

    add result to member number

--- LibraryLending.Domain/Aggregates/Members/ValueObjects/MemberNumbers/MemberNumberErrors.cs
+++ LibraryLending.Domain/Aggregates/Members/ValueObjects/MemberNumbers/MemberNumberErrors.cs
@@ -1,7 +1,9 @@
-﻿namespace LibraryLending.Domain.Aggregates.Members.ValueObjects.MemberNumbers;
+﻿using LibraryLending.SharedKernel.Results;
+
+namespace LibraryLending.Domain.Aggregates.Members.ValueObjects.MemberNumbers;
 
 public class MemberNumberErrors
 {
-    public const string Empty = "Medlemsnummer får inte vara tomt";
-    public static string TooLong(int maxLength) => $"Medlemsnummer får inte vara längre än {maxLength} tecken";
+    public static Error Empty => new(ErrorType.Validation, "Medlemsnummer får inte vara tomt");
+    public static Error TooLong(int maxLength) => new(ErrorType.Validation, $"Medlemsnummer får inte vara längre än {maxLength} tecken");
 }
