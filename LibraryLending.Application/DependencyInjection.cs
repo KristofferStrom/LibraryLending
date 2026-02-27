5f4ccd0af2dcb6576731e7f3473f492c50de41b6
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-02-27 15:36:29 +0100

    register handlers and decorators with scrutor

---
+++ LibraryLending.Application/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using LibraryLending.Application.Core.Abstractions.Messaging;
+using LibraryLending.Application.Core.Abstractions.Validations;
+using LibraryLending.Application.Core.Messaging;
+using LibraryLending.Application.Core.Pipelines.Validations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryLending.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IDispatcher, Dispatcher>();
+
+        services.Scan(scan => scan
+            .FromAssemblies(typeof(DependencyInjection).Assembly)
+
+            .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<,>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+
+            .AddClasses(c => c.AssignableTo(typeof(IQueryHandler<,>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+
+            .AddClasses(c => c.AssignableTo(typeof(IValidator<>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+        );
+
+        services.Decorate(typeof(ICommandHandler<,>), typeof(ValidationCommandDecorator<,>));
+        services.Decorate(typeof(IQueryHandler<,>), typeof(ValidationQueryDecorator<,>));
+
+        return services;
+    }
+}
