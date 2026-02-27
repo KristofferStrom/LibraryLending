27ec707e372e794e1e92e71ed022d4d6b5b281d6
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-02-27 15:22:52 +0100

    add core abstractions for cqrs add dispatcher

---
+++ LibraryLending.Application/Core/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using LibraryLending.SharedKernel.Results;
+
+namespace LibraryLending.Application.Core.Abstractions.Messaging;
+
+public interface ICommandHandler<in TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : Result
+{
+    Task<TResponse> Handle(TCommand command, CancellationToken ct);
+}
---
+++ LibraryLending.Application/Core/Abstractions/Validations/IValidator.cs
@@ -0,0 +1,8 @@
+﻿using LibraryLending.SharedKernel.Results;
+
+namespace LibraryLending.Application.Core.Abstractions.Validations;
+
+public interface IValidator<in TRequest>
+{
+    Error? Validate(TRequest request);
+}
