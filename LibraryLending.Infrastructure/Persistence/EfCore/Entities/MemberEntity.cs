04276da461f9ad66460d1699283d8409dc4783a4
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-03-02 13:20:58 +0100

    add memberEntity and config.

---
+++ LibraryLending.Infrastructure/Persistence/EfCore/Entities/MemberEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryLending.Infrastructure.Persistence.EfCore.Entities;
+
+internal sealed class MemberEntity
+{
+    public Guid Id { get; set; }
+
+    public string MemberNumber { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+}
