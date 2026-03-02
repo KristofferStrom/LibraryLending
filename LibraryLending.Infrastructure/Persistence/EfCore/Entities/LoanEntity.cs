98b95d84bb4884ff53193d57110fb594f6891a60
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-03-02 13:13:11 +0100

    add loanEntity and config

---
+++ LibraryLending.Infrastructure/Persistence/EfCore/Entities/LoanEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryLending.Infrastructure.Persistence.EfCore.Entities;
+
+internal sealed class LoanEntity
+{
+    public Guid Id { get; set; }
+
+    public Guid MemberId { get; set; }
+    public Guid BookCopyId { get; set; }
+
+    public DateTime LoanDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnedAt { get; set; }
+}
