fcf028ba05e9a5325d65ae9d3bfd003ad93fe53f
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-03-02 14:11:38 +0100

    add DI for infrastructure

--- LibraryLending.Infrastructure/DependencyInjection.cs
+++ LibraryLending.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,34 @@
-﻿namespace LibraryLending.Infrastructure;
+﻿using LibraryLending.Domain.Aggregates.Books.BookCopies;
+using LibraryLending.Domain.Aggregates.LendingPolicies;
+using LibraryLending.Domain.Aggregates.Loans;
+using LibraryLending.Domain.Aggregates.Members;
+using LibraryLending.Domain.Shared.Abstractions;
+using LibraryLending.Infrastructure.Persistence.EfCore.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyInjection
+namespace LibraryLending.Infrastructure;
+
+
+public static class ServiceCollectionExtensions
 {
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Default");
+
+        services.AddDbContext<LibraryDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        services.AddScoped<IMemberRepository, MemberRepository>();
+        services.AddScoped<IBookCopyRepository, BookCopyRepository>();
+        services.AddScoped<ILoanRepository, LoanRepository>();
+        services.AddScoped<ILendingPolicyRepository, LendingPolicyRepository>();
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
 }
