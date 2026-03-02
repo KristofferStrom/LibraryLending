d365db93072060173b0b533ab4c494b5b842a431
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-03-02 13:09:24 +0100

    add lending policy entity and configuration

---
+++ LibraryLending.Infrastructure/Persistence/EfCore/Configurations/LendingPolicyConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using LibraryLending.Infrastructure.Persistence.EfCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LibraryLending.Infrastructure.Persistence.EfCore.Configurations;
+
+internal sealed class LendingPolicyConfiguration : IEntityTypeConfiguration<LendingPolicyEntity>
+{
+    public void Configure(EntityTypeBuilder<LendingPolicyEntity> builder)
+    {
+        builder.ToTable("LendingPolicies");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.LoanDays)
+            .IsRequired();
+
+        builder.Property(x => x.IsActive)
+            .IsRequired();
+
+        builder.HasIndex(x => x.IsActive)
+            .HasFilter("[IsActive] = 1")
+            .IsUnique();
+    }
+}
---
+++ LibraryLending.Infrastructure/Persistence/EfCore/Entities/LendingPolicyEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryLending.Infrastructure.Persistence.EfCore.Entities;
+
+internal sealed class LendingPolicyEntity
+{
+    public Guid Id { get; set; }
+    public int LoanDays { get; set; }
+    public bool IsActive { get; set; }
+}
