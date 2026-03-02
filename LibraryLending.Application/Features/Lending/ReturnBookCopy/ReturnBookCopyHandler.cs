4c09fd20d2eadee282d0c2de92fbc605f73cf287
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-03-02 10:50:32 +0100

    add command add handler interface-methods for repos

---
+++ LibraryLending.Application/Features/Lending/ReturnBookCopy/ReturnBookCopyHandler.cs
@@ -0,0 +1,41 @@
+﻿using LibraryLending.Application.Core.Abstractions.Messaging;
+using LibraryLending.Domain.Aggregates.Books.BookCopies;
+using LibraryLending.Domain.Aggregates.Books.BookCopies.CopyBarcodes;
+using LibraryLending.Domain.Aggregates.Loans;
+using LibraryLending.Domain.Shared.Abstractions;
+using LibraryLending.SharedKernel.Results;
+
+namespace LibraryLending.Application.Features.Lending.ReturnBookCopy;
+
+public sealed class ReturnBookCopyHandler(
+    IBookCopyRepository bookCopies,
+    ILoanRepository loans,
+    IUnitOfWork unitOfWork
+)
+    : ICommandHandler<ReturnBookCopyCommand, Result>
+{
+    public async Task<Result> Handle(ReturnBookCopyCommand command, CancellationToken ct)
+    {
+        var barcodeResult = CopyBarcode.Create(command.CopyBarcode);
+        if (barcodeResult.IsFailure) return barcodeResult.Error;
+
+        BookCopy? bookCopy = await bookCopies.GetByBarcodeAsync(barcodeResult.Value, ct);
+        if (bookCopy is null) return ReturnBookCopyErrors.BookCopyNotFound;
+
+        Loan? loan = await loans.GetActiveByBookCopyIdAsync(bookCopy.Id, ct);
+        if (loan is null) return ReturnBookCopyErrors.ActiveLoanNotFound;
+
+        var returnResult = loan.Return(DateTime.UtcNow);
+        if (returnResult.IsFailure) return returnResult.Error;
+
+        var markAvailableResult = bookCopy.MarkAvailable();
+        if (markAvailableResult.IsFailure) return markAvailableResult.Error;
+
+        loans.UpdateReturnedAt(loan.Id, loan.ReturnedAt!.Value);
+        bookCopies.UpdateStatus(bookCopy.Id, bookCopy.Status);
+
+        await unitOfWork.SaveChangesAsync(ct);
+
+        return Result.Success();
+    }
+}
--- LibraryLending.Domain/Aggregates/Loans/ILoanRepository.cs
+++ LibraryLending.Domain/Aggregates/Loans/ILoanRepository.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryLending.Domain.Aggregates.Books;
+using LibraryLending.Domain.Aggregates.Books.BookCopies;
 using LibraryLending.Domain.Aggregates.Members;
 
 namespace LibraryLending.Domain.Aggregates.Loans;
@@ -6,5 +7,7 @@ namespace LibraryLending.Domain.Aggregates.Loans;
 public interface ILoanRepository
 {
     void Add(Loan loan);
+    Task<Loan?> GetActiveByBookCopyIdAsync(BookCopyId id, CancellationToken ct);
     Task<IReadOnlyCollection<BookId>> GetActiveLoanBookIdsForMemberAsync(MemberId id, CancellationToken ct);
+    void UpdateReturnedAt(LoanId id, DateTime value);
 }
