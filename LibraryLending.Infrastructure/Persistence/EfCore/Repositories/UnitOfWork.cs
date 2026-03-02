6beb7f87d4fb2f881e7e68b1ad5a138070cca994
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-03-02 14:23:11 +0100

    add assemblyInfo so that internal (domain) include infrastructure add bookCopyrepository and UnitOfWork

---
+++ LibraryLending.Infrastructure/Persistence/EfCore/Repositories/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using LibraryLending.Domain.Shared.Abstractions;
+using LibraryLending.Infrastructure.Persistence.EfCore.Contexts;
+
+namespace LibraryLending.Infrastructure.Persistence.EfCore.Repositories;
+
+internal sealed class UnitOfWork(LibraryDbContext db) : IUnitOfWork
+{
+    public Task SaveChangesAsync(CancellationToken ct) =>
+        db.SaveChangesAsync(ct);
+}
