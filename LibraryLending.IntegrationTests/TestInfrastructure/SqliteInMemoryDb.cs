9b838b422ba3538bfa22d82c220f2590d1a5910a
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2026-03-03 14:06:32 +0100

    add SqliteInMemoryDb and reference to infrastructure

---
+++ LibraryLending.IntegrationTests/TestInfrastructure/SqliteInMemoryDb.cs
@@ -0,0 +1,33 @@
+﻿using LibraryLending.Infrastructure.Persistence.EfCore.Contexts;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryLending.IntegrationTests.TestInfrastructure;
+
+
+internal sealed class SqliteInMemoryDb : IAsyncDisposable
+{
+    private readonly SqliteConnection _connection;
+    public LibraryDbContext Db { get; }
+
+    public SqliteInMemoryDb()
+    {
+        _connection = new SqliteConnection("Filename=:memory:");
+        _connection.Open();
+
+        var options = new DbContextOptionsBuilder<LibraryDbContext>()
+            .UseSqlite(_connection)
+            .EnableSensitiveDataLogging()
+            .Options;
+
+        Db = new LibraryDbContext(options);
+
+        Db.Database.EnsureCreated();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await Db.DisposeAsync();
+        await _connection.DisposeAsync();
+    }
+}
